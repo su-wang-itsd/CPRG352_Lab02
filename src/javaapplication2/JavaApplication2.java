5eada8590831a73116d33be40ebf275d1fba51b5
845593 <845593@SAIT227780IT.ACDM.DS.SAIT.CA>
2021-09-16 11:40:26 -0600

    Lab02 Practice One line print line of a sentence

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author 845593
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("thie is for the lab02 of CPRG352 at SAIT");
+        // TODO code application logic here
+    }
+    
+}
